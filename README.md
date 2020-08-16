9ddd38442617185bd7c6cb7b88433686eacc578b
Adiber <adiber.yt@gmail.com>
2020-08-16 10:48:01 +0200

    :sparkles: Clanstats
    Also changed rate limits

--- README.md
+++ README.md
@@ -14,17 +14,93 @@ There is no official API for retrieving player stats from the GommeHD.net Minecr
 The API responds with JSON objects.
 
 ### Playerstats
-#### 📍 Endpoint
-`GET api/v1/stats/player/PLAYER`
-<br>Where PLAYER is the Name of the Player
-#### 💌 Response:
+<b>You send: </b>PLAYER as the Minecraft Username of the Player
+
+#### 📜 Request
+```
+GET /api/v1/stats/player/PLAYER
+Content-Type: application/json
+Content-Length: xy
+```
+
+#### ⭐ Successful Response: 
 ```json
 {
   "message": "Results for PLAYER",
-  "data": {...}
+  "data": {
+    "ttt": {},
+    "skywars": {},
+    ...
+  }
 }
 ```
-⛔ If Player does not exist
+
+#### ⛔ If Player does not exist
+```json
+{
+  "message": "No entries found",
+  "data": {}
+}
+```
+
+### Clanmembers
+<b>You send: </b>CLANNAME as the Name of the GommeHD.net Clan
+
+#### 📜 Request
+```
+GET /api/v1/stats/clan/members/CLANNAME
+Content-Type: application/json
+Content-Length: xy
+```
+
+#### ⭐ Successful Response: 
+```json
+{
+  "message": "Members for CLANNAME",
+  "data": {
+    "leader": [],
+    "mods": [],
+    "member": [],
+  }
+}
+```
+
+#### ⛔ If Clan does not exist
+```json
+{
+  "message": "No entries found",
+  "data": {}
+}
+```
+
+### Clanwars
+<b>You send: </b>CLANNAME as the Name of the GommeHD.net Clan
+
+#### 📜 Request
+```
+GET /api/v1/stats/clan/wars/CLANNAME
+Content-Type: application/json
+Content-Length: xy
+```
+
+#### ⭐ Successful Response: 
+```json
+{
+  "message": "Wars for CLANNAME",
+  "data": {
+    "bw": {
+      "rank": "",
+      "points": "",
+      "wins": "",
+      "losses": "",
+    },
+    "cores": {...},
+    "sg": {...},
+  }
+}
+```
+
+#### ⛔ If Clan does not exist
 ```json
 {
   "message": "No entries found",
@@ -39,4 +115,4 @@ The API responds with JSON objects.
 
 ## Todo
 - [x] Playerstats
-- [ ] Clanstats
+- [x] Clanstats
--- src/api/stats.js
+++ src/api/stats.js
@@ -6,16 +6,16 @@ const slowDown = require("express-slow-down");
 
 const router = express.Router();
 
-const BASE_URL = 'https://www.gommehd.net/';
- 
+const BASE_URL = 'https://www.gommehd.net';
+
 const limiter = rateLimit({
     windowMs: 1 * 60 * 1000, // 1 minutes
-    max: 25 // limit each IP to 25 requests per minute
+    max: 100 // limit each IP to 25 requests per minute
 });
 
 const speedLimiter = slowDown({
     windowMs: 20 * 1000, // x seconds
-    delayAfter: 10, // allow x requests, then...
+    delayAfter: 50, // allow x requests, then...
     delayMs: 300 // begin adding delay per request
 });
 
@@ -31,7 +31,7 @@ router.get('/player/:username', limiter, speedLimiter, async (req, res, next) =>
         body = await body.text();
         const json = getStats(html2json(body));
 
-        if(Object.keys(json).length === 0) {
+        if (!json || Object.keys(json).length === 0) {
             return res.json({
                 message: 'No entries found',
                 data: {},
@@ -43,26 +43,99 @@ router.get('/player/:username', limiter, speedLimiter, async (req, res, next) =>
             message: `Results for ${req.params.username}`,
             data: json,
         })
-    }catch(err) {
+    } catch (err) {
         return next(err);
     }
-    
+
 });
 
-router.get('/clan', (req, res) => {
+router.get('/clan/members/:clanname', limiter, speedLimiter, async (req, res, next) => {
 
-})
+    try {
+        //1. get members
+        //URLs: https://www.gommehd.net/clan-profile/members-list?clanName=Higher
+
+        const params = new URLSearchParams({
+            clanName: req.params.clanname,
+        });
+        let body = await fetch(`${BASE_URL}/clan-profile/members-list?${params}`);
+        body = await body.text();
+        const json = getMembers(html2json(body));
+
+        if (!json || Object.keys(json).length === 0) {
+            return res.json({
+                message: 'No entries found',
+                data: {},
+            })
+        }
+
+        //2. respond to request
+        return res.json({
+            message: `Members for ${req.params.clanname}`,
+            data: json,
+        })
+    } catch (err) {
+        return next(err);
+    }
+
+});
+
+router.get('/clan/wars/:clanname', limiter, speedLimiter, async(req, res, next) => {
+
+    try {
+        //1. get clanwars
+        //URL: https://www.gommehd.net/clan-profile/wars/Higher?game=${game} //bedwars/cores/survivalgames
+
+        //bedwars
+        let params = new URLSearchParams({
+            game: 'bedwars',
+        });
+        let body = await fetch(`${BASE_URL}/clan-profile/wars/${req.params.clanname}?${params}`);
+        body = await body.text();
+        let json = getClanStats(html2json(body), 'bw');
+        //cores
+        params = new URLSearchParams({
+            game: 'cores',
+        });
+        body = await fetch(`${BASE_URL}/clan-profile/wars/${req.params.clanname}?${params}`);
+        body = await body.text();
+        json = {...json, ...getClanStats(html2json(body), 'cores')};
+        //sg
+        params = new URLSearchParams({
+            game: 'survivalgames',
+        });
+        body = await fetch(`${BASE_URL}/clan-profile/wars/${req.params.clanname}?${params}`);
+        body = await body.text();
+        json = {...json, ...getClanStats(html2json(body), 'sg')};
+
+        if (!json || Object.keys(json).length === 0) {
+            return res.json({
+                message: 'No entries found',
+                data: {},
+            })
+        }
+
+        //2. respond to request
+        return res.json({
+            message: `Wars for ${req.params.clanname}`,
+            data: json,
+        })
+    } catch (err) {
+        return next(err);
+    }
+
+});
 
 function getStats(obj) {
     obj = obj.child[0].child.filter(c => c.tag === 'body')[0].child.filter(c => (c.attr ? c.attr.id === 'content' : false))[0].child[1].child[3].child;
-    let res = {}
+    const res = {}
 
     obj.forEach(element => {
-        if(element.attr && element.attr.class.includes('stat-table')) {
+        if (element.attr && element.attr.class.includes('stat-table')) {
             const game = element.attr.id;
             res[game] = {}
             element.child[1].child[3].child[1].child.forEach(g => {
-                if(g.tag === "li") {
+                if (g.tag === "li") {
                     const score = g.child[1].child[0].text;
                     const type = g.child[4].text;
 
@@ -73,6 +146,51 @@ function getStats(obj) {
     });
 
     return res;
+}
+
+function getMembers(obj) {
+    const res = {}
+
+    obj.child.forEach(e => {
+        if(e.attr && e.attr.class.includes('panel')) {
+            const rank = switchRank(e.child.filter(c => c.attr?c.attr.class.includes('panel-heading'):false)[0].child[1].child[0].text);
+            res[rank] = []
+
+            e.child.filter(c => c.attr?c.attr.class.includes('panel-body'):false)[0].child[1].child.forEach(m => {
+                if(m.attr && m.attr.class.includes('media')) {
+                    const player = m.child[3].child[1].child[0].text.replace('\n', '').trim();
+                    res[rank].push(player)
+                }
+            })
+        }
+    })
+
+    return res;
+}
+
+function getClanStats(obj, game) {
+    obj = obj.child[0].child.filter(c => c.tag === 'body')[0].child.filter(c => (c.attr ? c.attr.id === 'content' : false))[0].child[1].child[1].child[3].child[1].child[3].child[1];
+
+    const rank = switchRank(obj.child[1].child[1].child[1].child[0].text.trim());
+    const points = obj.child[3].child[1].child[1].child[0].text.trim();
+    const wins = obj.child[1].child[3].child[1].child[0].text.trim();
+    const losses = obj.child[3].child[3].child[1].child[0].text.trim();
+
+    const res = {};
+    res[game] = {
+            rank, points, wins, losses
+    };
+    return res;
+}
+
+function switchRank(rank) {
+    switch(rank) {
+        case "Clan Leader": return "leader";
+        case "Clan Mods": return "mods";
+        case "Clan Member": return "member";
+        case "Keiner": return null;
+        default: return rank.substring(1);
+    }
 }
 
 module.exports = router;
